544dd8b9ec67699b70f7a32c4b142582224c501f
Nadia <nadia_dj@hotmail.co.uk>
2022-11-10 16:45:49 +0000

    guessing game project

---
+++ src/main/java/GuessingGame.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        //user input -> ask question Guess my number
+        //user inputs number
+        //if else statement -> if number == myNum print You guessed correct!
+        // else if -> too high
+        //else -> too low
+
+        //variable with my age
+        byte myNum = 24;
+        byte usersGuess = 0;
+        //prompt
+        System.out.println("How old do you think I am?");
+
+        //user input
+        Scanner user= new Scanner (System.in);
+        usersGuess = user.nextByte();
+
+//        System.out.println("Your guess is: " + usersGuess);
+
+
+        //if else statement
+        if(usersGuess == 24){
+            System.out.println("You guessed correct! Well done!");
+        }
+        else if(usersGuess > 24 ) {
+            System.out.println("You guessed too high :(");
+        }
+        else{
+            System.out.println("You guessed too low :(");
+        }
+
+    }
+}
